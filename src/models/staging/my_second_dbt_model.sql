71a851da1b9acf69e78fcb7ff21750e59e8da4b9
Hung Dang <hd@y42.com>
2023-03-30 17:31:04 +0200

    delete unused files

--- src/models/staging/my_second_dbt_model.sql
+++ src/models/staging/my_second_dbt_model.sql
@@ -4,3 +4,53 @@
 select *
 from {{ref('my_first_dbt_model') }}
 where id = 2
+
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ref('my_first_dbt_model') }}
+where id = 2
+x

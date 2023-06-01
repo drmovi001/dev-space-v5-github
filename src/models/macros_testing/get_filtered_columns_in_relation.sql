8296f757b3d7818c617fe93056ece9c13be73b2f
Hai Bui <hai.bui@y42.com>
2023-06-01 18:14:20 +0200

    d

---
+++ src/models/macros_testing/get_filtered_columns_in_relation.sql
@@ -0,0 +1,17 @@
+
+{% set
+    columns=dbt_utils.get_filtered_columns_in_relation(
+      from=ref('customers'),
+      except=['CUSTOMERID']
+    )
+    %}
+
+
+with source_data as (
+    select "{{columns}}" as id
+)
+
+
+SELECT
+  *
+FROM source_data

bbdca7e7a8bed49e7744ee033294ab5478aa9e3e
Hai Bui <hai.bui@y42.com>
2023-06-01 19:33:57 +0200

    fsd

--- src/models/macros_testing/generate_series.sql
+++ src/models/macros_testing/generate_series.sql
@@ -3,6 +3,6 @@ select
   {{ dbt_utils.pivot(
       'name',
       dbt_utils.get_column_values(ref('hai_customers'), 'name')
-  ) }}
+  ) }} as fds
 from {{ ref('hai_customers') }}
 group by customerid

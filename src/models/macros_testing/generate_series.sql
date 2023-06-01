bc9ccfb67e77a8623bb0f167ccbbe890a936e554
Hai Bui <hai.bui@y42.com>
2023-06-01 19:24:32 +0200

    sdaf

--- src/models/macros_testing/generate_series.sql
+++ src/models/macros_testing/generate_series.sql
@@ -1,4 +1,6 @@
-select 
-{{ dbt_utils.generate_surrogate_key(['customerid', 'name']) }} as bo
-from {{ref("hai_customers")}}
 
+
+select 
+{{ dbt_utils.safe_divide("orderid", "customerid") }} as one
+
+from {{ref("hai_orders")}}

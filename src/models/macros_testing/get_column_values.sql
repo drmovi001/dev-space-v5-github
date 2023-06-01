1db5ecac779f79422797136cfaf800d6ad9cb034
Hai Bui <hai.bui@y42.com>
2023-06-01 17:26:21 +0200

    change query

--- src/models/macros_testing/get_column_values.sql
+++ src/models/macros_testing/get_column_values.sql
@@ -1,3 +1,46 @@
-{% set some_columns = dbt_utils.get_column_values(
-        table=ref('hai_customers')
-) %}
+-- macros/get_column_values_from_query.sql
+{% macro get_column_values_from_query(query, column) -%}
+
+{#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. #}
+    {%- if not execute -%}
+        {{ return('') }}
+    {% endif %}
+
+    {% set column_values_sql %}
+    with cte as (
+        {{ query }}
+    )
+    select
+        {{ column }} as value
+
+    from cte
+    group by 1
+    order by count(*) desc
+
+    {% endset %}
+
+    {%- set results = run_query(column_values_sql) %}
+    {{ log(results, info=True) }}
+    {% set results_list = results.columns[0].values() %}
+
+    {{ log(results_list, info=True) }}
+    {{ return(results_list) }}
+
+{%- endmacro %}
+
+{% set data_query %}
+SELECT 'S' as size, 'red' as color
+UNION ALL SELECT 'S' ,'blue'
+UNION ALL SELECT 'S', 'red'
+UNION ALL SELECT 'M', 'red'
+{% endset %}
+
+with data as (
+    {{ data_query }}
+)
+
+SELECT
+    size,
+    {{ dbt_utils.pivot('color', get_column_values_from_query(data_query, 'color')) }}
+FROM data
+GROUP BY size

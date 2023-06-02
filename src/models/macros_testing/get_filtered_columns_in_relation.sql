aafe8a0b57c90c006317b49cd4b4645d1abd0b1f
Hai Bui <hai.bui@y42.com>
2023-06-02 16:25:46 +0200

    fsadf

--- src/models/macros_testing/get_filtered_columns_in_relation.sql
+++ src/models/macros_testing/get_filtered_columns_in_relation.sql
@@ -9,7 +9,7 @@
 with source_data as (
   select 
   {% for payment_method in column_names %}
-    "{{payment_method}}" as id{{loop.index}}
+    `{{payment_method}}` as id{{loop.index}}
 {% endfor %})
 
 

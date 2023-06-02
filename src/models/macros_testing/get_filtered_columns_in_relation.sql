405fe8226ffe787b0a5822476e81c93045c35c68
Hai Bui <hai.bui@y42.com>
2023-06-02 16:22:14 +0200

    fsadf

--- src/models/macros_testing/get_filtered_columns_in_relation.sql
+++ src/models/macros_testing/get_filtered_columns_in_relation.sql
@@ -1,14 +1,16 @@
 
 {% set
-    columns = dbt_utils.get_filtered_columns_in_relation(
+    column_names = dbt_utils.get_filtered_columns_in_relation(
       from=ref('hai_customers')
     )
     %}
 
 
 with source_data as (
-    select "{{columns}}" as id
-)
+  select 
+  {% for payment_method in column_names %}
+    {{payment_method}} as id{{loop.index}}
+{% endfor %})
 
 
 SELECT

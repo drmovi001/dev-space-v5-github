ad242345b4857a7ca8e994d352ed5fe0c951c53c
Hai Bui <hai.bui@y42.com>
2023-06-05 23:26:47 +0200

    asfsd

--- src/models/dbt_audithelper_testing/compare_column_values2.sql
+++ src/models/dbt_audithelper_testing/compare_column_values2.sql
@@ -1,6 +1,14 @@
+{% set old_etl_relation_query %}
+    select * from {{ref('hai_customers')}}
+{% endset %}
+
+{% set new_etl_relation_query %}
+    select * from {{ ref('hai_customers') }}
+{% endset %}
+
 {% set audit_helper = audit_helper.compare_column_values(
-    a_query=select * from {{ ref('hai_customers') }},
-    b_query=select * from {{ ref('hai_customers') }},
+    a_query=old_etl_relation_query,
+    b_query=new_etl_relation_query,
     primary_key='customerid',
     column_to_compare='country'
 ) %}

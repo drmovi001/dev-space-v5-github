11b5aa4706e9a120f38af17d434d66c8c33f6279
Hai Bui <hai.bui@y42.com>
2023-06-05 23:23:00 +0200

    sdf

--- src/models/dbt_audithelper_testing/compare_column_values2.sql
+++ src/models/dbt_audithelper_testing/compare_column_values2.sql
@@ -9,8 +9,8 @@
 {% set audit_helper = audit_helper.compare_column_values(
     a_query=old_etl_relation_query,
     b_query=new_etl_relation_query,
-    primary_key="customerid",
-    column_to_compare="country"
+    primary_key='customerid',
+    column_to_compare='country'
 ) %}
 
 {{ audit_query }}

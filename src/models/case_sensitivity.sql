5bc502b5741597075d564520252b995f03ecef54
Ba Thien <bathien.tran@y42.com>
2023-07-21 11:09:40 -0400

    change query

--- src/models/case_sensitivity.sql
+++ src/models/case_sensitivity.sql
@@ -1,1 +1,1 @@
-SELECT "some_LONG_NAME" AS "some_long_NAME" FROM {{ source('native_quoting', 'DBT_HAI_BUI_NATIVE_MODEL_QUOTED_LOWERCASE') }}
+SELECT "some_LONG_NAME" FROM {{ source('native_quoting', 'DBT_HAI_BUI_NATIVE_MODEL_QUOTED_LOWERCASE') }}

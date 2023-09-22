4626a897f704deaac0e1e69d068796fd11dedade
Hung Dang <hd@y42.com>
2023-09-22 20:05:03 +0200

    cleaning uo

--- src/models/my_second_dbt_model.sql
+++ src/models/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 -- Use the `ref` function to select from other models
-select
+SELECT
     *,
-    'test' as test_value
-from {{ ref('my_first_dbt_model') }}
-where id = 2
+    'test' AS test_value
+FROM {{ ref('my_first_dbt_model') }}
+WHERE id = 2
--- src/models/other/egi_tests/egi_test_pass.sql
+++ src/models/other/egi_tests/egi_test_pass.sql
@@ -1,9 +1,33 @@
-with customers as (
-    SELECT 1 AS customerid, 'Customer A' AS name, 'Address A' AS address, 'A' as country UNION ALL
-    SELECT 2 AS customerid, 'Customer B' AS name, 'Address B' AS address, 'B' as country UNION ALL
-    SELECT 3 AS customerid, 'Customer C' AS name, 'Address C' AS address, 'C' as country UNION ALL
-    SELECT 4 AS customerid, 'Customer D' AS name, 'Address D' AS address, 'B' as country UNION ALL
-    SELECT 5 AS customerid, 'Customer E' AS name, 'Address E' AS address, 'A' as country
+WITH customers AS (
+    SELECT
+        1 AS customerid,
+        'Customer A' AS name,
+        'Address A' AS address,
+        'A' AS country
+    UNION ALL
+    SELECT
+        2 AS customerid,
+        'Customer B' AS name,
+        'Address B' AS address,
+        'B' AS country
+    UNION ALL
+    SELECT
+        3 AS customerid,
+        'Customer C' AS name,
+        'Address C' AS address,
+        'C' AS country
+    UNION ALL
+    SELECT
+        4 AS customerid,
+        'Customer D' AS name,
+        'Address D' AS address,
+        'B' AS country
+    UNION ALL
+    SELECT
+        5 AS customerid,
+        'Customer E' AS name,
+        'Address E' AS address,
+        'A' AS country
 )
 
-select * FROM customers
+SELECT * FROM customers
